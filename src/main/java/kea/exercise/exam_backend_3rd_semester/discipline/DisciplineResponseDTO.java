912a9770f4ba6adacddb9bf8d9d4b376e41c1e7b
Chris Skov Sørensen <chso0002@stud.kea.dk>
2024-06-26 19:17:55 +0200

    added participant name to responseDTO

--- src/main/java/kea/exercise/exam_backend_3rd_semester/discipline/DisciplineResponseDTO.java
+++ src/main/java/kea/exercise/exam_backend_3rd_semester/discipline/DisciplineResponseDTO.java
@@ -2,10 +2,13 @@ package kea.exercise.exam_backend_3rd_semester.discipline;
 
 import kea.exercise.exam_backend_3rd_semester.resultType.ResultType;
 
+import java.util.List;
+
 public record DisciplineResponseDTO(
         Long id,
         String name,
         DisciplineType disciplineType,
-        ResultType resultType
+        ResultType resultType,
+        List<String> participants
 ) {
 }
